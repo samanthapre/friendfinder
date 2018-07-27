405599ecd546c76f15ed3b0f804c229f4172cd08
Samantha Prestidge <samanthaprestidge@er-10-129-133-85.wireless.umn.edu>
2018-07-27 14:29:44 -0500

    friends

---
+++ app/data/friends.js
@@ -0,0 +1,102 @@
+var friends = [
+    {
+      name: "Jimmy Butler",
+      photo: "http://rubasket.com/images/joomleague/players/4/659-Jimmy-Butler2.jpg",
+      scores: [
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+      ]
+    },
+    {
+      name: "Jeff Teague",
+      photo: " ",
+      scores: [
+        "4",
+        "2",
+        "5",
+        "1",
+        "3",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2"
+      ]
+    },
+    {
+      name: "Karl Anthony-Towns",
+      photo: " ",
+      scores: [
+        "5",
+        "2",
+        "2",
+        "2",
+        "4",
+        "1",
+        "3",
+        "2",
+        "5",
+        "5"
+      ]
+    },
+    {
+      name: "Andrew Wiggins",
+      photo: " ",
+      scores: [
+        "3",
+        "3",
+        "4",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2",
+        "2",
+        "3"
+      ]
+    },
+    {
+      name: "Tyus Jones",
+      photo: " ",
+      scores: [
+        "4",
+        "3",
+        "4",
+        "1",
+        "5",
+        "2",
+        "5",
+        "3",
+        "1",
+        "4"
+      ]
+    },
+    {
+      name: "Justin Patton",
+      photo: " ",
+      scores: [
+        "4",
+        "4",
+        "2",
+        "3",
+        "2",
+        "2",
+        "3",
+        "2",
+        "4",
+        "5"
+      ]
+    }
+  
+  ];
+  
+  // export the array
+  module.exports = friends;
